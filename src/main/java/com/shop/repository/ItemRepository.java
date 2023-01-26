c9d41226c9a08c76d2aff71e231711050e8d6a10
uch0731 <uch0731@naver.com>
2023-01-26 09:49:08 +0900

    1

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,15 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
